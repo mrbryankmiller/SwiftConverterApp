6cb3078ab957b10e6aae3568657329e439681bcc
Bryan Miller <bryanmiller@bryans-mbp.attlocal.net>
2016-07-31 23:15:23 -0500

    Converter App
    practice

--- SwiftConverterApp/ViewController.swift
+++ SwiftConverterApp/ViewController.swift
@@ -8,7 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
+    
+    
+    @IBOutlet weak var milesField: UITextField!
+    
+    
+    
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    
+
+    @IBAction func convertPressed(sender: UIButton) {
+    
+    
+    //1 mile= 5280 feet
+    //get the value from text field
+    //convert text into Float 
+    //Do calculation  for conversion
+    //se the result labet 
+    
+    let miles = Float (milesField.text!)
+    let feets = miles! * 5280.0
+    resultsLabel.text = String(feets)
+    
+        
+    
+
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
